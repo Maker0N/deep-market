83c5a3170a2c3c9493e6bafa88d1236482b0acfa
Maker0N <makerov@e1.ru>
2022-04-24 12:36:22 +0500

    merge auth-reg

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -14,4 +14,16 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:itemId', async (req, res) => {
+  try {
+    const { itemId } = req.params
+    const item = await Product.findById(itemId)
+    res.send(item)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error on server. Try leter',
+    })
+  }
+})
+
 module.exports = router
